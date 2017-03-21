441d7e9118c07e8c396af91d8efd1da7fc6a2a86
Pi <pi@fullstack.it>
2017-03-21 20:13:33 +0000

    first commit

---
+++ lib/arskall.ex
@@ -0,0 +1,18 @@
+defmodule Arskall do
+  @moduledoc """
+  Documentation for Arskall.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Arskall.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/arskall_test.exs
@@ -0,0 +1,8 @@
+defmodule ArskallTest do
+  use ExUnit.Case
+  doctest Arskall
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
